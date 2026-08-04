89f2b4db8e02f9c8feb54dc56ea6c0321f6d197f
donghao058 <donghao058@ke.com>
2026-08-04 11:55:09 +0800

    收紧云托管接口访问边界

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const crypto = require("crypto");
 const express = require("express");
-const cors = require("cors");
 const morgan = require("morgan");
 const { Op } = require("sequelize");
 const {
@@ -17,7 +16,6 @@ const app = express();
 app.disable("x-powered-by");
 app.use(express.urlencoded({ extended: false, limit: "1mb" }));
 app.use(express.json({ limit: "1mb" }));
-app.use(cors());
 app.use(morgan("tiny"));
 
 function getOpenId(req) {
@@ -824,10 +822,6 @@ app.delete("/api/sync", requireWeddingMember, requireOwner, async (req, res, nex
     await transaction.rollback();
     next(error);
   }
-});
-
-app.get("/api/wx_openid", requireWeChatUser, (req, res) => {
-  res.send({ code: 0, data: { openid: req.openid } });
 });
 
 app.use((error, req, res, next) => {
